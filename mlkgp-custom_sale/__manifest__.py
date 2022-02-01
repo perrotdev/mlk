6b8e0358489b05b9f57c1b73a828f99ee0f9b258
Daniel Perrot <support@perrot.tech>
2022-02-01 21:05:45 +0100

    FIX: sale module name

--- mlkgp-custom_sale/__manifest__.py
+++ mlkgp-custom_sale/__manifest__.py
@@ -16,11 +16,11 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'sale',
+    'category': 'Sales',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['Sales'],
+    'depends': ['sale'],
 
     # always loaded
     'data': [
