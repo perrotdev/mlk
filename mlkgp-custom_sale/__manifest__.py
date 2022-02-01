2a40f62f39ca0d51058c322eeba2094d89281cca
Daniel Perrot <support@perrot.tech>
2022-02-01 20:30:06 +0100

    ADD : Visualisation de l'opportunite liee à un devis

--- mlkgp-custom_sale/__manifest__.py
+++ mlkgp-custom_sale/__manifest__.py
@@ -3,24 +3,24 @@
     'name': "mlkgp-custom_sale",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Personnalisation du module Sale pour MLK Grand Paris""",
 
     'description': """
-        Long description of module's purpose
+        Personnalisation du module Sale pour MLK Grand Paris :
+        - Associé un devis à une opportunité sans le mode debug - modification possble uniquement par une manager.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "PERROTTECH",
+    'website': "http://www.perrot.tech",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Sales',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['Sales'],
 
     # always loaded
     'data': [
