fc8a1aa1ba8dc6f9e701f9bf0330a5c5991fd631
Daniel Perrot <support@perrot.tech>
2026-07-17 00:38:22 +0200

    Delete module mlkgp-custom_sale

---
+++ mlkgp_migration/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'MLKGP Migration',
+    'version': '19.0.1.0.0',
+    'category': 'Technical',
+    'summary': 'Scripts de migration pour MLKGP',
+    'description': """
+        Module technique contenant les scripts de migration pour MLKGP
+    """,
+    'author': 'PERROTTECH',
+    'depends': ['base'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
